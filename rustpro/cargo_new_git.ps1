f2fb0e1d8aa0fe364edbaf24872ff78cf0471668
fgwsz <3191000458@qq.com>
2023-12-12 16:39:55 +0800

    change cargo_new_git

--- rustpro/cargo_new_git.ps1
+++ rustpro/cargo_new_git.ps1
@@ -5,6 +5,5 @@
 $project_name=Read-Host -Prompt "input project name"
 cargo new $project_name --vcs git
 if(Test-Path -Path $project_name){
-    cd $project_name
-    Remove-Item .git -Force -Recurse # 删除子项目文件夹中的.git文件，保证主目录git push不会失败
+    Remove-Item $project_name/.git -Force -Recurse # 删除子项目文件夹中的.git文件，保证主目录git push不会失败
 }
