f092630c5480a89f923d1c74a2ab8f9e43eee645
fgwsz <3191000458@qq.com>
2023-12-12 16:46:55 +0800

    修正一处错误

--- rustpro/statements/src/main.rs
+++ rustpro/statements/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     //let x=(let y=6);//error,使用let的是一个语句，而不是一个表达式
     //let x=let y=6;//error
     let y={
-        let x=3;//表达式是没有返回值的
+        let x=3;//语句是没有返回值的
         x+1//注意这里没有;表示这是一个表达式，而不是语句
     };
     println!("The value of y is:{y}");//The value of y is:4
