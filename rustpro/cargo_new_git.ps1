5f2870bea17c7ff1f0e6fa4eaa3eceb02ce8a5f8
fgwsz <3191000458@qq.com>
2023-12-12 16:34:17 +0800

    change cargo_new_git

--- rustpro/cargo_new_git.ps1
+++ rustpro/cargo_new_git.ps1
@@ -4,3 +4,7 @@
 # 所以本脚本用来代替cargo new命令快速创建rust git项目,并简化了cargo new --vcs git命令输入操作
 $project_name=Read-Host -Prompt "input project name"
 cargo new $project_name --vcs git
+if(Test-Path -Path $project_name){
+    cd $project_name
+    Remove-Item .git -Force -Recurse # 删除子项目文件夹中的.git文件，保证主目录git push不会失败
+}
